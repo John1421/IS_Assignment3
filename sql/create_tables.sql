ba269ab72cdcafc926bfedecc2d997ca57e49286
Zan Pizmoht <42890960+zanp98@users.noreply.github.com>
2024-12-11 12:15:58 +0000

    Add sql for creating operator

---
+++ sql/create_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `operator` (
+  `name` varchar(255) NOT NULL, 
+  PRIMARY KEY (`name`)
+);
+
+
+INSERT INTO operator VALUES ("SMTUC");
+INSERT INTO operator VALUES ("Bolt");
