04d5802206a8997509ee1ceb963302dd077a9414
João Samuel Diogo <joaosamueldiogo@gmail.com>
2024-12-17 15:30:19 +0000

    sh scalability

--- config/create-source.sh
+++ config/create-source.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json" --data @config/source.json http://connect:8083/connectors
+# Check if the filename is provided as a command-line argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
+# Use the provided filename
+FILENAME=$1
+
+# Make the POST request with the provided filename
+curl -X POST -H "Content-Type: application/json" --data @config/"$FILENAME"-source.json http://connect:8083/connectors
