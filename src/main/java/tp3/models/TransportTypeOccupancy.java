34c51b47d1cca5fc68bebcb65f54625149ad98c1
Zan Pizmoht <42890960+zanp98@users.noreply.github.com>
2024-12-20 15:12:27 +0000

    cluster container fixes

--- src/main/java/tp3/models/TransportTypeOccupancy.java
+++ src/main/java/tp3/models/TransportTypeOccupancy.java
@@ -2,9 +2,11 @@ package tp3.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransportTypeOccupancy {
     private String transportType;
     private Float occupancyPercentage;
